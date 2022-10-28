377170f3ea32c7cb08feb56a4d8fddd63f3754bf
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-28 18:43:51 +0300

    auth, provider, signInWithGoogle added to LoginContainer

--- whatsapp/components/LoginContainer.js
+++ whatsapp/components/LoginContainer.js
@@ -1,15 +1,20 @@
 import { Button} from '@mui/material'
+import { bgcolor, Container } from '@mui/system'
 import Image from 'next/image'
 import React from 'react'
+import { auth, provider,signInWithGoogle } from '../firebase'
 
 const LoginContainer = () => {
+
 return (
-    <div className='flex flex-col'>
-        <img className='h-[200px] w-[200px]'
+    <Container  sx={{display:"grid", placeItems:"center",height:"100vh",bgcolor:"whitesmoke"}} >
+    <div className='flex flex-col items-center bg-white p-[100px] shadow-[0_4px_14px_-3px_rgba(0,0,0,0.7)]'>
+        <img className='h-[200px] w-[200px] mb-[50px]'
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png'
         />
-        <Button variant='outlined'>Sign in with Google</Button>
+        <Button onClick={signInWithGoogle} variant='outlined'>Sign in with Google</Button>
     </div>
+    </Container>
 )
 }
 
