2ce3bf6ee5a1fcde646d2817e4d4e7c20f471b8e
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-28 18:44:12 +0300

    Update Header.js

--- whatsapp/components/Header.js
+++ whatsapp/components/Header.js
@@ -2,12 +2,13 @@ import { Avatar, IconButton } from '@mui/material'
 import ChatIcon from '@mui/icons-material/Chat'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import React from 'react'
+import { auth } from '../firebase';
 
 const Header = () => {
     return (
         <header className='flex sticky top-0 bg-white z-0 z-1 justify-between
         items-center p-4 h-20 border-b border-solid'>
-            <Avatar alt='Ahmet Palavan' className='cursor-pointer hover:opacity-80' src='/static/images/avatar/1.jpg'/>
+            <Avatar onClick={()=>auth.signOut()} alt='Ahmet Palavan' className='cursor-pointer hover:opacity-80' src='/static/images/avatar/1.jpg'/>
             <iconscontainer>
                 <IconButton>
                     <ChatIcon/>
