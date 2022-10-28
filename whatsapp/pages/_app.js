bc736fcd11d6835ef395b21b36095d1408d59502
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-28 18:45:00 +0300

    loading and user added to _app.js

--- whatsapp/pages/_app.js
+++ whatsapp/pages/_app.js
@@ -1,10 +1,27 @@
 import '../styles/globals.css'
 import {useAuthState} from 'react-firebase-hooks/auth'
-import { auth } from '../firebase'
+import { auth, db } from '../firebase'
 import Login from './login'
+import { Loading } from '../components'
+import { collection, onSnapshot } from 'firebase/firestore'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth)
+  const [user,loading] = useAuthState(auth);
+  useEffect(()=>{
+    if(user){
+      onSnapshot(collection(db,"users"),(snapshot)=>{
+        (snapshot.docs.map((user)=>{
+          return{
+            email: user.email,
+            lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+            photoURL: user.photoURL
+          },{merge:true}
+        }))
+      })
+    }
+  },[user])
+  if(loading) return <Loading/>
   if(!user) return <Login/>
   return <Component {...pageProps} />
 }
