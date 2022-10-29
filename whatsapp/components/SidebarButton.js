23d016091dc806364838baa6f73007c9fca283fb
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-29 16:54:06 +0300

    update sidebarbutton

--- whatsapp/components/SidebarButton.js
+++ whatsapp/components/SidebarButton.js
@@ -4,16 +4,23 @@ import * as EmailValidator from 'email-validator'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
 import { addDoc, collection } from 'firebase/firestore';
+import {useCollection} from 'react-firebase-hooks/firestore'
 const SidebarButton = () => {
     const [user] = useAuthState(auth);
+    const userChatRef = collection(db,"chats",`{user.email}`,`chats`)
+    const [chatsSnapshot] = useCollection(userChatRef);
     const createChat = () =>{
         const input = prompt('Please enter an email address for the user you wish to chat with');
         if(!input) return null;
-        if(EmailValidator.validate(input)) {
+        if(EmailValidator.validate(input) && !chatAlreadyExists(input) && input !== user.email) {
+            // we need to add the chat into the DB 'chats' collection if it doesn't already exist and is valid
             addDoc(collection(db,'chats'),{
                 users:[user.email,input]
             })
         }
+    }
+    const chatAlreadyExists = (recipientEmail) => {
+        !!chatsSnapshot?.docs.find((chat) => chat.data().users.find((user) => user === recipientEmail)?.length > 0)
     }
 return (
     <div>
