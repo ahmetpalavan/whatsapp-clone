f131e8201b33b92a9d06931459ca65ebb0c75645
ahmetpalavan <ahmetpalavann@gmail.com>
2022-10-30 00:14:26 +0300

    Update SidebarButton.js

--- whatsapp/components/SidebarButton.js
+++ whatsapp/components/SidebarButton.js
@@ -3,13 +3,13 @@ import {Button} from "@mui/material"
 import * as EmailValidator from 'email-validator'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection,where } from 'firebase/firestore';
 import {useCollection} from 'react-firebase-hooks/firestore'
 import Chat from './Chat';
-const SidebarButton = () => {
+    const SidebarButton = () => {
     const [user] = useAuthState(auth);
-    const userChatRef = collection(db,"chats",`{user.email}`,`chats`)
-    const [chatsSnapshot] = useCollection(userChatRef);
+    const userChatRef = collection(db, 'chats')
+    const [chatsSnapshot] = useCollection(userChatRef, where ('users', 'array-contains', user));
     const createChat = () =>{
         const input = prompt('Please enter an email address for the user you wish to chat with');
         if(!input) return null;
@@ -21,12 +21,16 @@ const SidebarButton = () => {
         }
     }
     const chatAlreadyExists = (recipientEmail) => {
-        !!chatsSnapshot?.docs.find((chat) => chat.data().users.find((user) => user === recipientEmail)?.length > 0)
+        !!chatsSnapshot?.docs.find(chat => chat.data().users.find((user) => user === recipientEmail)?.length > 0)
     }
 return (
     <div>
         <Button onClick={createChat} className='w-full border-t-[1px] border-b-[1px] text-black border-solid border-[#f5f5f5]'>Start a new chat</Button>
-        <Chat/>
+        {
+            chatsSnapshot?.docs.map(chat => ( 
+                <Chat key={chat.id} id={chat.id} users={chat.data().users}/>
+            ))
+        }
     </div>
 )
 }
